f0dd564a2af4dca74d0824ae47a0f981efa9953d
Catia Pinto <root@localhost.localdomain>
2016-08-09 13:11:34 +0200

    changes the port number3

--- Server.java
+++ Server.java
@@ -13,7 +13,7 @@ public class Server {
 
     public static void main(String[] args) throws IOException {
 
-        ServerSocket serverSocket = new ServerSocket(8092);
+        ServerSocket serverSocket = new ServerSocket(8093);
 
         while (true) {
 
