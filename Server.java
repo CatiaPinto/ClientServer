abfc26d9185f2e753b543a72eb4054d72bd02e11
CatiaPinto <catia.mcspinto@outlook.com>
2016-08-09 17:15:05 +0200

    Testing build on jenkins, changes the port to 8096

--- Server.java
+++ Server.java
@@ -13,7 +13,7 @@ public class Server {
 
     public static void main(String[] args) throws IOException {
 
-        ServerSocket serverSocket = new ServerSocket(8095);
+        ServerSocket serverSocket = new ServerSocket(8096);
 
         while (true) {
 
